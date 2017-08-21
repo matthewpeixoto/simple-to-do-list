872f319836953f98086ba187f6bd1067d0d7bdfa
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-08-21 15:56:16 -0400

    Complete program

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 $form = $('form');
+$item = $('.item');
+$list = $('.list');
+
+$form.on('submit', function (e) {
+  var $newItem = $('<li>').html($item.val(''));
+
+  e.preventDefault();
+  $list.append($newItem);
+});
+
+$list.on('click', 'li', function (e) {
+  $(this).toggleClass('js-complete');
+});
